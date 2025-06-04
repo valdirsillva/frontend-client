3ae6aad04273b726bf6ce1a0dfc5d9453d6502ae
Valdir Correia <valdir.developervs@gmail.com>
2025-06-04 15:27:57 -0300

    Initial commit from Create Next App

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
+  staticDirs: ['../public'],
+  stories: ['../src/components/**/stories.tsx'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: (config) => {
+    config.resolve?.modules?.push(`${process.cwd()}/src`)
+    return config
+  }
+}
+export default config
