525492cb92d70af0beef0f08d17aa0ee38a822bb
Valdir Silva <valdirpiresba@gmail.com>
2025-07-05 17:58:25 -0300

    feat: add component BannerSlider

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import BannerSlider, { BannerSliderProps } from '.'
+
+const items = [
+  {
+    img: 'https://images.pexels.com/photos/414837/pexels-photo-414837.jpeg',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://images.pexels.com/photos/580900/pexels-photo-580900.jpeg',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryFn<BannerSliderProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <BannerSlider {...args} />
+  </div>
+)
